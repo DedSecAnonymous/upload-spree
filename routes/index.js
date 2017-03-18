963ae7f9d09eceea5a7ad00462c2958b8d253a5c
mhtkmr54 <mhtkmr54@gmail.com>
2017-03-18 19:05:48 +0000

    minor

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TDP' });
+  res.render('index', { title: 'Microsoft Code.Fun.Do' });
 });
 
 module.exports = router;
